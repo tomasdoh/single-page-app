b62814aa3a1f2f6c51c135b8551f66b8b0fbc4d0
Tomas Doherty <tomasdoh1@gmail.com>
2018-12-11 14:32:14 +0000

    Added script to index.html
    Note text renders on screen when submitted. Not working with app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+submitNote();
 
 function submitNote() {
   document
-    .getElementById("note-submit")
-    .addEventListener("click", function(clickEvent){
-      clickEvent.preventDefault()
+  .getElementById("note-submit")
+  .addEventListener("click", function(clickEvent){
+      clickEvent.preventDefault();
       showNote();
-    })
+  })
 };
 
-function showNote(text) {
-  document
-    .getElementById("note-text")
-    .innerHTML = text
+function showNote() {
+  var note = document.getElementById("note-text").value
+  document.getElementById("note").innerHTML = note
 };
